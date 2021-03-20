2079a656ff107667215af9e93846a481530d934e
Santiago Cardona <santiagocardo80@gmail.com>
2021-03-19 20:31:39 -0500

    🚀 Deploy: Testing Heroku Server

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,9 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :carpooling, CarpoolingWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  http: [port: {:system, "PORT"}],
+  url: [scheme: "https", host: "transportesolidario.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
@@ -53,3 +55,5 @@ config :logger, level: :info
 # Finally import the config/prod.secret.exs which loads secrets
 # and configuration from environment variables.
 import_config "prod.secret.exs"
+
+config :carpooling, Carpooling.Repo, types: Carpooling.PostgresTypes
--- lib/carpooling_web/endpoint.ex
+++ lib/carpooling_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule CarpoolingWeb.Endpoint do
   ]
 
   socket "/socket", CarpoolingWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
